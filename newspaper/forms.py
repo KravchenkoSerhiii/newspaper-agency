9d57ff7a767acbaaae2eb65a65a0bad3ef3c699a
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-05-12 10:56:57 +0200

    Create search field for all models

--- newspaper/forms.py
+++ newspaper/forms.py
@@ -30,3 +30,30 @@ class RedactorInfoUpdateForm(forms.ModelForm):
     class Meta:
         model = Redactor
         fields = ["first_name", "last_name", "years_of_experience", "email"]
+
+
+class RedactorSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"}),
+    )
+
+
+class NewspaperSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by title"}),
+    )
+
+
+class TopicSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
+    )
